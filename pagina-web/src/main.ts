0ce84fc4a72e169afa827f98215aae0ba337b845
CaroCe <carito_kt@hotmail.com>
2019-07-30 18:28:19 -0500

    vistas-login-registro

--- pagina-web/src/main.ts
+++ pagina-web/src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {join} from "path";
+import * as express from 'express';
+import {NestExpressApplication} from "@nestjs/platform-express";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule) as NestExpressApplication;
+  app.setViewEngine('ejs');
+  app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  app.use(express.static('publico'));
   await app.listen(3000);
+
 }
 bootstrap();
